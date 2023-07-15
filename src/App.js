2c2716726801baef093f247c5fb53c6bf5536343
M.N. Jeral Sandeeptha <jeral.sandeeptha1@gmail.com>
2023-07-15 17:33:29 +0530

    Bug fix

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import OneTimePayment from './pages/OneTimePayment';
+import Success from './pages/Success';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/onetimepayment' element={<OneTimePayment/>}/>
+        <Route path='/success' element={<Success/>}/>
       </Routes>
     </BrowserRouter>
   );
